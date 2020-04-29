f1ca8350b5b2fb1aded2a706cf18faf4ded66ced
tonyfang <q710370@gmail.com>
2020-04-29 16:50:33 +0800

    Update authenticated

--- src/apis/admin.js
+++ src/apis/admin.js
@@ -1,16 +1,24 @@
 import { apiHelper } from "./../utils/helpers";
-
+const getToken = () => localStorage.getItem("token");
 export default {
   getCreateProduct() {
-    return apiHelper.get("/admin/CreateProduct");
+    return apiHelper.get("/admin/CreateProduct", {
+      headers: { Authorization: `Bearer ${getToken()}` },
+    });
   },
   getStore() {
-    return apiHelper.get("/admin/store");
+    return apiHelper.get("/admin/store", {
+      headers: { Authorization: `Bearer ${getToken()}` },
+    });
   },
   createProduct({ formData }) {
-    return apiHelper.post("/admin/NewProduct", formData);
+    return apiHelper.post("/admin/NewProduct", formData, {
+      headers: { Authorization: `Bearer ${getToken()}` },
+    });
   },
   createActivity({ formData }) {
-    return apiHelper.post("/admin/NewActivity", formData);
+    return apiHelper.post("/admin/NewActivity", formData, {
+      headers: { Authorization: `Bearer ${getToken()}` },
+    });
   },
 };
