16525c55531c6aca8609acfd3d468bfed5252258
tonyfang <q710370@gmail.com>
2020-05-03 15:39:17 +0800

    Update deploy

--- deploy.sh
+++ deploy.sh
@@ -16,11 +16,9 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f git@github.com:tonyfang1992/master-cat-fe.git master:gh-pages
+git push -f git@github.com/tonyfang1992/master-cat-fe.git master:gh-pages
 
 cd -
